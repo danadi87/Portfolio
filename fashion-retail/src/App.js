749b312513c9fc37d64a0e6cb6a7965166417f80
danadi87 <dragulescumirela@gmail.com>
2023-07-09 18:55:15 +0200

    added a carousel on the main page

--- fashion-retail/src/App.js
+++ fashion-retail/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Header";
+import Mainpagebody from "./Mainpagebody.js";
 
 export default function App() {
   return (
     <div className="App">
       <div className="App-header">
         <Header />
+        <Mainpagebody />
       </div>
     </div>
   );
