737c4d376b687e8396922ea48aadce270c8843b6
danadi87 <dragulescumirela@gmail.com>
2023-07-27 12:38:19 +0200

    new carousel attempt

--- fashion-retail/src/Carousel.js
+++ fashion-retail/src/Carousel.js
@@ -1,75 +1,19 @@
-import React, { useEffect, useState } from "react";
-import "./carousel.css";
-import { Children } from "react";
+import { useState } from "react";
+import { sculptureList } from "./data.js";
+import carousel from "./carousel.css";
 
-export const CarouselItem = ({ children, width }) => {
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <div className="carousel-item" style={{ width: width }}>
-      {children}
-    </div>
+    <>
+      <button onClick={handleClick}>Next</button> 
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
   );
-};
-const Carousel = ({ children }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const updateIndex = (newIndex) => {
-    if (newIndex < 0) {
-      newIndex = React.Children.count(children) - 1;
-    } else if (newIndex >= React.Children.count(children)) {
-      newIndex = 0;
-    }
-    setActiveIndex(newIndex);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      updateIndex(activeIndex + 1);
-    }, 3000);
-    return () => {
-      if (interval) {
-        clearInterval(interval);
-      }
-    };
-  });
-  return (
-    <div className="carousel">
-      <div
-        className="inner"
-        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
-      >
-        {React.Children.map(children, (child, index) => {
-          return React.cloneElement(child, { width: "100%" });
-        })}
-      </div>
-      <div className="indicators">
-        <button
-          onClick={() => {
-            updateIndex(activeIndex - 1);
-          }}
-        >
-          Prev
-        </button>
-        {React.Children.map(children, (child, index) => {
-          return (
-            <button
-              className={`${index === activeIndex ? "active" : ""}`}
-              onClick={() => {
-                updateIndex(index);
-              }}
-            >
-              {index + 1}
-            </button>
-          );
-        })}
-        <button
-          onClick={() => {
-            updateIndex(activeIndex + 1);
-          }}
-        >
-          Next
-        </button>
-      </div>
-    </div>
-  );
-};
-export default Carousel;
+}
